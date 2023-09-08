1900e93e185802e89c0892e61b887c8b162355e5
DrewBoschert <drew.r.boschert@gmail.com>
2023-09-08 08:58:59 -0400

    Add project files.

---
+++ Pig Dice/Program.cs
@@ -0,0 +1,30 @@
+﻿var random = new Random();
+
+for(int j = 0; j < 1000000000; j++)
+{
+
+
+var score = 0;
+var i = true;
+
+while (i == true)
+{
+    var rnd = random.Next(1, 7);
+    if(rnd != 1) { 
+        score += rnd;
+    }
+    
+    else
+    {
+        i = false;
+       // Console.WriteLine("GAME OVER");
+
+            if(score > 450)
+            {
+                Console.WriteLine("THIS ONE RIGHT HERE");
+                Console.WriteLine($"score: {score}");
+            }
+    }
+}
+    
+}
